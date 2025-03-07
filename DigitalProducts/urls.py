4fc05949b68bcea9504fd1a820c7215b711c02ce
measivs <meako.sivsivadze@gmail.com>
2025-03-07 20:28:49 +0400

    optimize templates

--- DigitalProducts/urls.py
+++ DigitalProducts/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
-    path('', include('categories.urls')),
+    # path('', include('categories.urls')),
 ]
